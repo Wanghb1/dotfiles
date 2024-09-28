b5b21c8b282805c426e0a859b22383df91ca3d40
wanghongbo <whbo@mail.ustc.edu.cn>
2024-09-28 22:53:48 +0800

    add tmux dotfile

--- install.sh
+++ install.sh
@@ -8,13 +8,24 @@ if ! $(ln -s $(pwd)/.bashrc ~/.bashrc); then
 	echo "create symbolic link ~/.bashrc fail"
 	exit 1
 fi
+source ~/.bashrc
 
 # install .vimrc
 if [ -e "$HOME/.vimrc" ]; then
-	echo "exist bash dotfile, remove it"
+	echo "exist vim dotfile, remove it"
 	rm -f ~/.vimrc 
 fi
 if ! $(ln -s $(pwd)/.vimrc ~/.vimrc); then 
 	echo "create symbolic link ~/.vimrc fail"
+	exit 1
+fi
+
+# install .tmux.conf
+if [ -e "$HOME/.tmux.conf" ]; then
+	echo "exist tmux dotfile, remove it"
+	rm -f ~/.tmux.conf 
+fi
+if ! $(ln -s $(pwd)/.tmux.conf ~/.tmux.conf); then 
+	echo "create symbolic link ~/.tmux.conf fail"
 	exit 1
 fi
